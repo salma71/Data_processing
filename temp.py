5bacbf778c3f44ece23ab8c0102793cd8c593152
Salma Elshahawy <salmaeng71@gmail.com>
2018-09-14 13:41:37 -0400

    feature scalling

--- temp.py
+++ temp.py
@@ -30,3 +30,7 @@ x = onehotencoder.fit_transform(x).toarray()
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+#splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
